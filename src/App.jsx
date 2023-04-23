e5bea44c389f14d70c030c2c859512795e893b1c
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-04-23 02:21:30 +0100

    openAI

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Demo from "./Demo.jsx";
+import Hero from "./Hero.jsx";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Demo.jsx
@@ -0,0 +1,94 @@
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "./assets";
+import { useLazyGetSummaryQuery } from "./services/article";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_iccon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          ></button>
+        </form>
+        {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] object-contain"
+                />
+              </div>
+              <p>{item.url}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* result */}
+    </section>
+  );
+};
+
+export default Demo;
